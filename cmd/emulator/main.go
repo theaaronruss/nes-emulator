bb397b8b8944f06837817c9ab4a9bd51c1524f91
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-08-30 23:26:33 -0400

    add basic graphics

--- cmd/emulator/main.go
+++ cmd/emulator/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/theaaronruss/nes-emulator/internal/bus"
-	"github.com/theaaronruss/nes-emulator/internal/cpu"
+	"github.com/gopxl/pixel/v2"
+	"github.com/gopxl/pixel/v2/backends/opengl"
 )
 
 func main() {
-	mainBus := bus.NewBus()
-	err := mainBus.LoadRom("test.nes")
-	if err != nil {
-		fmt.Println(err.Error())
-		return
+	opengl.Run(run)
+}
+
+func run() {
+	cfg := opengl.WindowConfig{
+		Title:  "NES Emulator",
+		Bounds: pixel.R(0, 0, 512, 448),
+		VSync:  true,
 	}
-	cpu := cpu.NewCpu(mainBus)
-	cpu.ClockCycle()
+	win, err := opengl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+	width := int(win.Canvas().Bounds().W())
+	height := int(win.Canvas().Bounds().H())
+	pixels := make([]uint8, 4*width*height)
+	for i := 0; i < cap(pixels); i += 4 {
+		pixels[i] = 0xFF
+		pixels[i+1] = 0x00
+		pixels[i+2] = 0xFF
+		pixels[i+3] = 0xFF
+	}
+	win.Canvas().SetPixels(pixels)
+	for !win.Closed() {
+		win.Update()
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/theaaronruss/nes-emulator
 
 go 1.24.5
+
+require github.com/gopxl/pixel/v2 v2.3.0
+
+require (
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
+	github.com/go-gl/mathgl v1.1.0 // indirect
+	github.com/gopxl/glhf/v2 v2.0.0 // indirect
+	github.com/gopxl/mainthread/v2 v2.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/image v0.19.0 // indirect
+)
