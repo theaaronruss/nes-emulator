0a20544928ea6bfc39b214fc878b25a0565db6e1
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-09-04 00:55:51 -0400

    update how graphics are drawn

--- cmd/emulator/main.go
+++ cmd/emulator/main.go
@@ -12,24 +12,25 @@ func main() {
 func run() {
 	cfg := opengl.WindowConfig{
 		Title:  "NES Emulator",
-		Bounds: pixel.R(0, 0, 512, 448),
-		VSync:  true,
+		Bounds: pixel.R(0, 0, 512, 480),
 	}
 	win, err := opengl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
-	width := int(win.Canvas().Bounds().W())
-	height := int(win.Canvas().Bounds().H())
-	pixels := make([]uint8, 4*width*height)
+	canvas := opengl.NewCanvas(pixel.R(0, 0, 256, 240))
+	pixels := make([]uint8, 4*256*240)
 	for i := 0; i < cap(pixels); i += 4 {
 		pixels[i] = 0xFF
 		pixels[i+1] = 0x00
 		pixels[i+2] = 0xFF
 		pixels[i+3] = 0xFF
 	}
-	win.Canvas().SetPixels(pixels)
+	canvas.SetPixels(pixels)
 	for !win.Closed() {
+		transformationMatrix := pixel.IM.Scaled(pixel.Vec{}, 2)
+		transformationMatrix = transformationMatrix.Moved(win.Bounds().Center())
+		canvas.Draw(win, transformationMatrix)
 		win.Update()
 	}
 }
