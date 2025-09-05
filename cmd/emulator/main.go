26a2e36ad51e717ffec908df566230087251acb6
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-09-04 20:40:55 -0400

    draw static from ppu

--- cmd/emulator/main.go
+++ cmd/emulator/main.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gopxl/pixel/v2"
 	"github.com/gopxl/pixel/v2/backends/opengl"
+	"github.com/theaaronruss/nes-emulator/internal/cartridge"
+	"github.com/theaaronruss/nes-emulator/internal/cpu"
+	"github.com/theaaronruss/nes-emulator/internal/ppu"
+	"github.com/theaaronruss/nes-emulator/internal/sysbus"
+)
+
+const (
+	frameWidth  float64 = 256
+	frameHeight float64 = 240
+	frameTime   int64   = 1000 / 60
 )
 
 func main() {
@@ -10,27 +23,44 @@ func main() {
 }
 
 func run() {
+	var err error
+	sysbus.GamePak, err = cartridge.LoadCartridge("nestest.nes")
+	if err != nil {
+		fmt.Println("Failed to load ROM file")
+		return
+	}
+	cpu := cpu.NewCpu()
+	ppu := ppu.NewPpu()
+	clockCycle := 0
+
 	cfg := opengl.WindowConfig{
 		Title:  "NES Emulator",
-		Bounds: pixel.R(0, 0, 512, 480),
+		Bounds: pixel.R(0, 0, frameWidth*2, frameHeight*2),
 	}
 	win, err := opengl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
 	canvas := opengl.NewCanvas(pixel.R(0, 0, 256, 240))
-	pixels := make([]uint8, 4*256*240)
-	for i := 0; i < cap(pixels); i += 4 {
-		pixels[i] = 0xFF
-		pixels[i+1] = 0x00
-		pixels[i+2] = 0xFF
-		pixels[i+3] = 0xFF
-	}
-	canvas.SetPixels(pixels)
 	for !win.Closed() {
+		startTime := time.Now()
+		for !ppu.FrameComplete {
+			ppu.ClockCycle()
+			clockCycle++
+			if clockCycle >= 3 {
+				cpu.ClockCycle()
+				clockCycle = 0
+			}
+		}
+		canvas.SetPixels(ppu.FrameBuffer)
+		ppu.FrameComplete = false
+
 		transformationMatrix := pixel.IM.Scaled(pixel.Vec{}, 2)
 		transformationMatrix = transformationMatrix.Moved(win.Bounds().Center())
 		canvas.Draw(win, transformationMatrix)
 		win.Update()
+		elapsedTime := time.Since(startTime).Milliseconds()
+		sleepTime := frameTime - elapsedTime
+		time.Sleep(time.Duration(sleepTime) * time.Millisecond)
 	}
 }
---
+++ internal/ppu/ppu.go
@@ -0,0 +1,33 @@
+package ppu
+
+import "math/rand"
+
+type Ppu struct {
+	FrameBuffer   []uint8
+	FrameComplete bool
+	pixelOffset   int
+}
+
+func NewPpu() *Ppu {
+	return &Ppu{
+		FrameBuffer: make([]uint8, 4*256*240),
+	}
+}
+
+func (p *Ppu) ClockCycle() {
+	var color uint8
+	if rand.Intn(2) == 1 {
+		color = 0xFF
+	} else {
+		color = 0x00
+	}
+	p.FrameBuffer[p.pixelOffset*4] = color
+	p.FrameBuffer[p.pixelOffset*4+1] = color
+	p.FrameBuffer[p.pixelOffset*4+2] = color
+	p.FrameBuffer[p.pixelOffset*4+3] = 0xFF
+	p.pixelOffset++
+	if p.pixelOffset >= 256*240 {
+		p.pixelOffset = 0
+		p.FrameComplete = true
+	}
+}
