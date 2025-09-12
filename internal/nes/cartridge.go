f2548ed2464414c68e84d05f1c90ea6d3103fd97
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-09-11 20:47:24 -0400

    add rom reading

---
+++ internal/nes/cartridge.go
@@ -0,0 +1,83 @@
+package nes
+
+import (
+	"errors"
+	"io"
+	"os"
+)
+
+const (
+	headerSize        int = 16
+	programDataSize   int = 16384
+	characterDataSize int = 8192
+)
+
+type Cartridge struct {
+	programData   []byte
+	characterData []byte
+}
+
+func NewCartridge(filePath string) (*Cartridge, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	cartridge := &Cartridge{}
+
+	err = cartridge.parseProgramData(file)
+	if err != nil {
+		return nil, err
+	}
+
+	err = cartridge.parseCharacterData(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return cartridge, nil
+}
+
+func (cartridge *Cartridge) MustRead(address uint16) uint8 {
+	if address > uint16(programDataSize) {
+		return cartridge.programData[address]
+	}
+	panic("game cartridge invalid address")
+}
+
+func (cartridge *Cartridge) parseProgramData(file *os.File) error {
+	_, err := file.Seek(int64(headerSize), io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	cartridge.programData = make([]byte, programDataSize)
+	n, err := file.Read(cartridge.programData)
+	if n != programDataSize {
+		return errors.New("unexpected end of file")
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cartridge *Cartridge) parseCharacterData(file *os.File) error {
+	_, err := file.Seek(int64(headerSize+programDataSize), io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	cartridge.characterData = make([]byte, characterDataSize)
+	n, err := file.Read(cartridge.characterData)
+	if n != characterDataSize {
+		return errors.New("unexpected end of file")
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- internal/nes/sysbus.go
+++ internal/nes/sysbus.go
@@ -1,16 +1,36 @@
 package nes
 
+const (
+	cpuRamSize int = 2048
+)
+
 type BusReadWriter interface {
 	Read(uint16) uint8
 	Write(uint16, uint8)
 }
 
 type SysBus struct {
+	cpuRam    [cpuRamSize]uint8
+	cartridge *Cartridge
+}
+
+func NewSysBus() *SysBus {
+	return &SysBus{}
+}
+
+func (bus *SysBus) InsertCartridge(cartridge *Cartridge) {
+	bus.cartridge = cartridge
 }
 
 func (bus *SysBus) Read(address uint16) uint8 {
+	if address < 0x0800 {
+		return bus.cpuRam[address]
+	}
 	return 0x00
 }
 
 func (bus *SysBus) Write(address uint16, data uint8) {
+	if address < 0x0800 {
+		bus.cpuRam[address] = data
+	}
 }
